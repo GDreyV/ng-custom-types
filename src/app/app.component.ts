542212dc2eee6258f9e7f4b0ba0e13bd8c0de791
Andrew Grebennikov <av.grebennikov@gmail.com>
2019-05-07 16:37:30 -0400

    add custom types

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+import * as host from 'host';
+const parsedUrl = host.parse('https://angular.io');
+console.log(parsedUrl.hostname);
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
